dbe68f0c53a08736e5c8d0374e15fe44a4b6a541
Muhammad Huzaifa <108292736+huzaifa654@users.noreply.github.com>
2023-09-22 05:37:01 -0700

    updated code

--- App.js
+++ App.js
@@ -3,13 +3,15 @@ import React from 'react'
 import ScrollAnimation from './ScrollAnimation'
 import ImagesAniamtion from './ImagesAniamtion'
 import VerticalScrollAnimation from './VerticalScrollAnimation'
+import CarouselAnimation from './CarouselAnimation'
 
 export default function App() {
   return (
-    <View style={{flex:1}}>
+    <View style={{ flex: 1 }}>
       {/* <ScrollAnimation/> */}
-      {/* <ImagesAniamtion/> */}
-      <VerticalScrollAnimation/>
+      {/* <ImagesAniamtion /> */}
+      {/* <VerticalScrollAnimation/> */}
+      <CarouselAnimation/>
     </View>
   )
 }
